c3be7e191f9d766dbcd74c02eadfea70567645db
John Keech <jokeech@microsoft.com>
2019-08-22 12:30:49 -0700

    Make install script work better

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 sudo unlink ~/.bashrc
-ln -sv ~/.dotfiles/.bashrc ~
+ln -sv $DIR/.bashrc ~
 
 sudo unlink ~/.vs-liveshare-settings.json
-ln -sv ~/.dotfiles/.vs-liveshare-settings.json ~
+ln -sv $DIR/.vs-liveshare-settings.json ~
 
 source ~/.bashrc
