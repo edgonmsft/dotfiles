23a676b110afb942c7f0029269380f634f9c9547
John Keech <jokeech@microsoft.com>
2021-05-28 20:03:50 +0000

    Make dotfiles install work again

--- install.sh
+++ install.sh
@@ -6,6 +6,3 @@ echo "(dotfiles) starting ..." >> ~/dotfiles_status
 cp .vs-liveshare-settings.json ~
 
 echo "(dotfiles) finished" >> ~/dotfiles_status
-
-echo "Intentionally failing dotfiles by exiting with a non-zero status!"
-exit 1
